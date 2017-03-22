33a141fa9495ca204d5185d72f7aef7079f0f6b9
acaozixu123456 <331004814@qq.com>
2017-03-22 13:04:45 +0800

    Initial commit

---
+++ src/com/cjjs/czx/HelloWorld.groovy
@@ -0,0 +1,14 @@
+package com.cjjs.czx
+/**
+ * Created by 曹子谞 on 2017/3/22.
+ */
+
+def str = "helloword";
+
+println(str);
+
+def list1 = ["张三","里斯","王五"];
+
+list1.each {
+    temp -> println(temp);
+}
